405d88f3ea003d62b5d45263c4cf9c5727ae3f56
ljh <3369661439@qq.com>
2024-05-03 20:41:29 +0800

    添加删除缓存逻辑

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -9,9 +9,11 @@ import com.sky.service.DishService;
 import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @RestController
@@ -20,11 +22,15 @@ public class DishController {
 
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @PostMapping
     public Result addDish(@RequestBody DishDTO dishDTO) {
         log.info("添加菜品: {}", dishDTO);
         dishService.addDish(dishDTO);
+        //清除缓存
+        redisTemplate.delete("dish_" + dishDTO.getCategoryId());
         return Result.success();
     }
 
@@ -39,6 +45,9 @@ public class DishController {
     public Result deleteDish(@RequestParam List<Long> ids) {
         log.info("批量删除菜品:{}", ids);
         dishService.deleteDish(ids);
+        //清除缓存
+        Set keys = redisTemplate.keys("dish_");
+        redisTemplate.delete(keys);
         return Result.success();
     }
 
@@ -63,30 +72,39 @@ public class DishController {
      */
     @PutMapping
     public Result updateDish(@RequestBody DishDTO dishDTO) {
+        log.info("修改菜品数据:{}",dishDTO);
         dishService.updateDish(dishDTO);
+        //清除缓存
+        Set keys = redisTemplate.keys("dish_");
+        redisTemplate.delete(keys);
         return Result.success();
     }
 
     /**
      * 菜品起售、停售
+     *
      * @param status
      * @param id
      * @return
      */
     @PostMapping("/status/{status}")
-    public Result updateDishStatus(@PathVariable Integer status,Long id) {
+    public Result updateDishStatus(@PathVariable Integer status, Long id) {
         log.info("菜品起售、停售:{}{}", status, id);
-        dishService.updateStatus(status,id);
+        dishService.updateStatus(status, id);
+        //清除缓存
+        Set keys = redisTemplate.keys("dish_");
+        redisTemplate.delete(keys);
         return Result.success();
     }
 
     /**
      * 根据菜品分类id查询数据
+     *
      * @param categoryId
      * @return
      */
     @GetMapping("/list")
-    public Result<List<Dish>> getDishList(Long categoryId ) {
+    public Result<List<Dish>> getDishList(Long categoryId) {
         log.info("根据菜品分类id查询数据:{}", categoryId);
         List<Dish> dishs = dishService.getDishByCategoryId(categoryId);
         return Result.success(dishs);
